3ef3799a01ffaeab6dc9d7036ef8663d6c30df6f
Arie Moldawsky <ariemol555@gmail.com>
2020-11-26 16:16:50 +0200

    backend booking

--- api/booking/booking.controller.js
+++ api/booking/booking.controller.js
@@ -25,13 +25,11 @@ async function deleteBooking(req, res) {
 async function addBooking(req, res) {
     var booking = req.body;
     // booking.byUserId = req.session.user._id;
-    if (req.session.user) {
         booking.guestUser = {
             _id: req.session.user._id,
             fullName: req.session.user.fullName,
             imgUrl: req.session.user.imgUrl
         };
-    }
     booking = await bookingService.add(booking)
     res.send(booking)
 }
